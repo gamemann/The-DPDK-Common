637b63eb5647dc8c842989049304b6fa1272316f
gamemann <gamemann@gflclan.com>
2021-11-20 23:07:59 +0000

    Include all DPDK headers inside of dpdk_common.h.

--- src/dpdk_common.c
+++ src/dpdk_common.c
@@ -10,27 +10,6 @@
 #include <getopt.h>
 #include <signal.h>
 
-#include <rte_common.h>
-#include <rte_log.h>
-#include <rte_malloc.h>
-#include <rte_memory.h>
-#include <rte_memcpy.h>
-#include <rte_eal.h>
-#include <rte_launch.h>
-#include <rte_cycles.h>
-#include <rte_prefetch.h>
-#include <rte_lcore.h>
-#include <rte_per_lcore.h>
-#include <rte_branch_prediction.h>
-#include <rte_interrupts.h>
-#include <rte_random.h>
-#include <rte_debug.h>
-#include <rte_ether.h>
-#include <rte_ethdev.h>
-#include <rte_mempool.h>
-#include <rte_mbuf.h>
-#include <rte_string_fns.h>
-
 #include "dpdk_common.h"
 
 volatile __u8 quit;
--- src/dpdk_common.h
+++ src/dpdk_common.h
@@ -1,6 +1,27 @@
 #ifndef DPDK_COMMON_HEADER
 #define DPDK_COMMON_HEADER
 
+#include <rte_common.h>
+#include <rte_log.h>
+#include <rte_malloc.h>
+#include <rte_memory.h>
+#include <rte_memcpy.h>
+#include <rte_eal.h>
+#include <rte_launch.h>
+#include <rte_cycles.h>
+#include <rte_prefetch.h>
+#include <rte_lcore.h>
+#include <rte_per_lcore.h>
+#include <rte_branch_prediction.h>
+#include <rte_interrupts.h>
+#include <rte_random.h>
+#include <rte_debug.h>
+#include <rte_ether.h>
+#include <rte_ethdev.h>
+#include <rte_mempool.h>
+#include <rte_mbuf.h>
+#include <rte_string_fns.h>
+
 #include <linux/types.h>
 
 /* Common defines */
